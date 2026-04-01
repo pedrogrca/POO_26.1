9db3072f37cf47283a7aa059d9e0ac6c5b727da9
Pedro Graça <68236129+pedrogrca@users.noreply.github.com>
2026-04-01 18:22:06 -0300

    Removido

--- aula.py
+++
@@ -1,9 +0,0 @@
-x = int(input())
-
-y = int(input())
-
-if x > y :
-    print("Oi")
-else:
-    print("Tchau")
-
--- aula2.py
+++
@@ -1,4 +0,0 @@
-# Q2
-x = 
-meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
-
