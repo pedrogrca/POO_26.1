838a0e49590277c8efed86f325e045ce958c99ba
Pedro Graça <68236129+pedrogrca@users.noreply.github.com>
2026-04-07 21:43:31 -0300

    Fixed logic problem and added user input with while instance

--- Encapsulamento - Lista 2/Q2.py
+++ Encapsulamento - Lista 2/Q2.py
@@ -1,24 +1,42 @@
 class Viagem:
     def __init__(self):
-        self.distancia = 0.0
-        self.tempo = 0.0
+        self.__distancia = 0.0
+        self.__tempo = 0.0
     def set_distancia(self, d):
-        self.distancia = d
+        if d <= 0:
+            print("Erro: A distância precisa ser maior que 0.")
+            return False
+        else:
+            self.__distancia = d 
+            return True
+            
     def set_tempo(self, t):
-        self.tempo = t
+        if t <= 0:
+            print("❌ Erro: O tempo precisa ser maior que 0.")
+            return False
+        else: 
+            self.__tempo = t
+            return True
+
     def get_distancia(self):
-        return self.distancia
+        return self.__distancia
     def get_tempo(self):
-        return self.tempo
+        return self.__tempo
     def velocidade_media(self):
-        return self.distancia / (self.tempo / 60)
-    
+        return self.__distancia / (self.__tempo / 60)
+
 x = Viagem()
 
-x.set_distancia(500)
-x.set_tempo(200)
+while True:
+    d = float(input("Digite o valor da distância (em KM): "))
+    valid = x.set_distancia(d)
+    if valid == True:
+        break
 
-print(x.get_distancia())
-print(x.get_tempo())
+while True:
+    t = float(input("Digite o valor do tempo (em minutos): "))
+    valid = x.set_tempo(t)
+    if valid == True:
+        break
 
-print(x.velocidade_media())
+print(f"A velocidade média é: {x.velocidade_media():.2f} km/h")
